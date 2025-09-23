db7ce6dc273012d33804407fe1935c025c0ea4d1
kimternet <kimternet23@gmail.com>
2025-09-23 17:40:30 +0900

    test

---
+++ posbackend/src/main/java/com/example/posbackend/PosbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.posbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosbackendApplication.class, args);
+	}
+
+}
